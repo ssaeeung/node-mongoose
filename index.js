3e6679712c972c5377d8033609b997320d178f7e
Torn <ajaree.eatlab.space>
2022-08-24 10:07:43 -0700

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+
+
+const url = 'mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url)
+
+connect.then((db) => {
+    console.log('Connected correctly to the server')
+
+    var newDish = Dishes({
+        name: 'TonyPizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log('dish',dish)
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log('dishes=', dishes)
+            return Dishes.deleteMany({})
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log('error=',err)
+        })
+});
